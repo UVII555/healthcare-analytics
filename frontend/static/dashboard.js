85509f627547c6fcbc4d5acfcaa88d60babbc1c3
Utsav Singh <singhutsav555@gmail.com>
2026-07-29 21:53:15 +0530

    dashboard.js/style.css

---
+++ frontend/static/dashboard.js
@@ -0,0 +1,18 @@
+function saveToken() {
+    const token = document.getElementById('tokenInput').ariaValueMax.trim();
+    localStorage.setItem('medconnect_token',token);
+    loadALL();
+}
+
+function getToken() { return localStorage.getItem('medconnect_token') || '';}
+function authHeaders() {
+    const token = getToken() {
+        return token ? ('Authorization': 'Bearer $(token)') : ();
+    }
+
+
+function logError(msg) {
+    const el = document.getElementById('errorLog');
+    
+}
+}
